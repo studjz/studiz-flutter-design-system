1137700508eb4d8221a07385270117aadceac1ef
유지우 <yy7826w@gmail.com>
2026-03-05 00:09:44 +0900

    add color design system.

---
+++ styles/colors.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+abstract final class StudizColors {
+  static const Color main = Color(0xFF60C4FF);
+  static const Color mainDarker = Color(0xFF1987C9);
+  static const Color mainLighter = Color(0xFFA9DFFF);
+  static const Color mainSubtle = Color(0xFFC8D9E3);
+
+  static const Color primary = Color(0xFF3377FF);
+  static const Color secondary = Color(0xFFF8CE5A);
+
+  static const Color info = Color(0xFF2F80ED);
+  static const Color success = Color(0xFF27AE60);
+  static const Color warning = Color(0xFFE2B93B);
+  static const Color error = Color(0xFFEB5757);
+
+  static const Color black1 = Color(0xFF000000);
+  static const Color black2 = Color(0xFF1D1D1D);
+  static const Color black3 = Color(0xFF282828);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color gray1 = Color(0xFF333333);
+  static const Color gray2 = Color(0xFF4F4F4F);
+  static const Color gray3 = Color(0xFF828282);
+  static const Color gray4 = Color(0xFFBDBDBD);
+  static const Color gray5 = Color(0xFFE0E0E0);
+}
